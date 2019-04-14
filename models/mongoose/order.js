8ff94dafcf7be30a67a693b7ceb7f3997563d3d6
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-04-14 18:54:48 +0100

    Update the order model to remove name

--- models/mongoose/order.js
+++ models/mongoose/order.js
@@ -8,7 +8,6 @@ const orderSchema = new Schema({
 		}
 	],
 	user: {
-		name: { type: String, required: true },
 		email: { type: String, required: true },
 		userId: {
 			type: Schema.Types.ObjectId,
