d16a5fa3329c191e920c99c8b0de59f430b792d7
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-04-14 13:27:30 +0100

    Add a new end point to logout

--- routers/auth.js
+++ routers/auth.js
@@ -1,10 +1,10 @@
 const express = require("express");
 
 const routes = express.Router();
-const {AuthController} = require("../controllers/mongoose/auth");
+const { AuthController } = require("../controllers/mongoose/auth");
 const ctrl = new AuthController();
 
-routes.get("/signin" , ctrl.index);
-routes.post("/signin" , ctrl.signin);
-
+routes.get("/signin", ctrl.index);
+routes.post("/signin", ctrl.signin);
+routes.get("/logout", ctrl.logout);
 exports.authRoutes = routes;
