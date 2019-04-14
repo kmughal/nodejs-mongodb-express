8517c171f28a3522f954b44a401d033cf5cb11a6
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-04-14 13:57:06 +0100

    redirect should only happen once the session is saved

--- controllers/mongoose/auth.js
+++ controllers/mongoose/auth.js
@@ -1,26 +1,27 @@
-const {cookieHelper} = require("../../common/cookie-helper");
+const { cookieHelper } = require("../../common/cookie-helper");
 
 exports.AuthController = class AuthController {
 	index(req, res, next) {
 		//const isAuthenticated = req.session.isAuthenticated;
-		console.log(req.session.isAuthenticated)
+		console.log(req.session.isAuthenticated);
 		const vm = {
 			path: "login",
 			title: "Sign in",
-			isAuthenticated : req.session.isAuthenticated
+			isAuthenticated: req.session.isAuthenticated
 		};
 		res.render("login/index", vm);
 	}
 
-	signin(req, res, next) {
-		res.setHeader("Set-Cookie" , "isAuthenticated=true; max-age=10;");
+	async signin(req, res, next) {
+		res.setHeader("Set-Cookie", "isAuthenticated=true; max-age=10;");
 		req.session.isAuthenticated = true;
 		req.session.user = req.user;
+		await req.session.save();
 		res.redirect("/auth/signin");
 	}
 
-	async logout(req,res,next) {
-		await req.session.destroy(()=> {
+	async logout(req, res, next) {
+		await req.session.destroy(() => {
 			res.redirect("/auth/signin");
 		});
 	}
