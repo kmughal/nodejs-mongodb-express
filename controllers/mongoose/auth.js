326baf45a20e236d6e6e45a3c5ca64453394dac6
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-04-19 14:09:06 +0100

    Add methods to handle password reset

--- controllers/mongoose/auth.js
+++ controllers/mongoose/auth.js
@@ -1,28 +1,22 @@
 const { cookieHelper } = require("../../common/cookie-helper");
 const { UserModel } = require("../../models/mongoose/user");
 const { passwordHelpers } = require("../../common/password-hashing");
-const nodeMailer = require("nodemailer");
-const sendgridTransport = require("nodemailer-sendgrid-transport");
-const {getSettings} = require("../../common/setting-helper");
-const settings =  getSettings("send-grid");
-const transporter = 
-nodeMailer.createTransport(sendgridTransport({
-	auth : {
-	 
-		api_key : settings.api_key
-	}
-}));
+const crypto = require("crypto");
+const { sendEmail } = require("../../common/send-email");
+const {toObjectId} = require("../../infrastructure/mongodb");
+
+
 exports.AuthController = class AuthController {
 	index(req, res, next) {
 		//const isAuthenticated = req.session.isAuthenticated;
 		const errorMessages = req.flash("error");
 		console.log("error:", errorMessages);
 		const vm = {
-			path: "login",
+			path: "auth",
 			title: "Sign in",
 			errorMessages
 		};
-		res.render("login/index", vm);
+		res.render("auth/signin", vm);
 	}
 
 	async signin(req, res, next) {
@@ -46,13 +40,13 @@ exports.AuthController = class AuthController {
 		req.session.isAuthenticated = true;
 		req.session.user = req.user;
 		await req.session.save();
-		console.log("send email")
-		await transporter.sendMail({
-			to : "kmughal@gmail.com",
-			from : "kmughal@gmail.com",
-			subject: "test email" ,
-			html:"hello from me"
-		});
+		console.log("send email");
+		await sendEmail(
+			"You are signed in",
+			"Just to let you know that you logged in",
+			"kmughal@gmail.com",
+			"kmughal@gmail.com"
+		);
 		res.redirect("/shop/product");
 	}
 
@@ -70,7 +64,7 @@ exports.AuthController = class AuthController {
 			isAuthenticated: false,
 			errorMessages
 		};
-		res.render("login/signup", vm);
+		res.render("auth/signup", vm);
 	}
 
 	async postSignup(req, res, next) {
@@ -91,6 +85,99 @@ exports.AuthController = class AuthController {
 			cart: { items: [] }
 		});
 		await newUser.save();
+		res.redirect("/auth/signin");
+	}
+
+	resetPassword(req, res, next) {
+		const errorMessages = req.flash("error");
+		res.render("auth/resetpassword", {
+			path: "resetpassword",
+			title: "Reset password",
+			errorMessages
+		});
+	}
+
+	async postResetPassword(req, res, next) {
+		try {
+			const { email } = req.body;
+			const user = await UserModel.findOne({ email });
+			if (!user) {
+				req.flash("error", `${email} not found!`);
+				return res.redirect("/auth/reset");
+			}
+
+			// Send email
+			const buffer = await crypto.randomBytes(32);
+			const token = buffer.toString("hex");
+			user.resetToken = token;
+			const dd = new Date();
+			dd.setHours(dd.getHours() + 1);
+			user.resetTokenExpiration = dd;
+			await user.save();
+
+			await sendEmail(
+				"Password reset request",
+				`
+				<p>You have requested to reset password.</p>
+				<p>Click this <a href='http://localhost:3000/auth/reset/${token}'>link</a> to continue with your request.</p>
+			`,
+				"kmughal@gmail.com",
+				"kmughal@gmail.com"
+			);
+			res
+				.status(200)
+				.send("An email has been sent for reset please check your inbox!");
+		} catch (e) {
+			console.log("PostResetPassword Error, e:", e);
+		}
+	}
+
+	async requestNewPassword(req, res, next) {
+		const { token } = req.params;
+
+		const user = await UserModel.findOne({
+			resetToken: token,
+			resetTokenExpiration: { $gt: Date.now() }
+		});
+		console.log("user req" , user)
+		if (typeof user === undefined) {
+			req.flash("error", "user not found");
+			return res.redirect("/auth/reset");
+		}
+		console.log("route:/auth/new-password/" ,user._id.toString(), "/",token)
+
+		res.render("auth/new-password", {
+			userId: user._id.toString(),
+			passwordToken: token,
+			path: "NewPassword",
+			title: "New Password",
+			errorMessages : []
+		});
+	}
+
+	async postNewPasswordRequest(req, res, next) {
+		const { userId, password, confirmPassword, passwordToken } = req.body;
+		const user = await UserModel.findOne({
+			resetToken: passwordToken,
+			resetTokenExpiration: { $gt: Date.now() },
+			_id: toObjectId(userId)
+		});
+	
+		if (!user) {
+			req.flash("error", "user not found!");
+			return res.redirect("/auth/reset");
+		}
+
+		user.password = await passwordHelpers.encrypt(password);
+		user.confirmPassword = await passwordHelpers.encrypt(confirmPassword);
+		user.resetToken = null;
+		user.resetTokenExpiration = null;
+		await user.save();
+		await sendEmail("Password changed successfully" ,`
+			<p> Dear ${user.email} , your password has been changed please click <a href='http://localhost:3000/auth/signin'> here </a> to sign in.</p>
+			<p> Regards , </p>
+			<h2>Mongo course team!</h2>
+		`,'kmughal@gmail.com' , 'kmughal@gmail.com');
 		res.redirect("/auth/signin");
 	}
 };
