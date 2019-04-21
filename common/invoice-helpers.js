6944447bf52918b727e06fd35a5962e34f4ac29a
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-04-21 15:05:34 +0100

    Add a method to delete file

--- common/invoice-helpers.js
+++ common/invoice-helpers.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-
 const PdfDocument = require("pdfkit");
 
 exports.generateInvoice = ({ orderId, products }) => {
@@ -14,7 +13,6 @@ exports.generateInvoice = ({ orderId, products }) => {
 	const pdf = new PdfDocument();
 	pdf.pipe(fs.createWriteStream(invoiceFileFullPath));
 
-	
 	pdf
 		.fontSize(8)
 		.text("Date:" + new Date().toLocaleDateString("en-gb"), 400, 10);
@@ -22,16 +20,20 @@ exports.generateInvoice = ({ orderId, products }) => {
 
 	let total = 0.0;
 	pdf.fontSize(10);
-	pdf.text("=================================",20);
+	pdf.text("=================================", 20);
 	products.forEach(prod => {
-		pdf.fontSize(11).text("Product name: " + prod.title,20);
-		pdf.fontSize(10).text("Quantity: " + prod.quantity,20);
-		pdf.fontSize(10).text("Unit price: " + prod.price,20);
-		total += parseFloat(prod.quantity) * parseFloat(prod.price,20);
-		pdf.text("=================================",20);
+		pdf.fontSize(11).text("Product name: " + prod.title, 20);
+		pdf.fontSize(10).text("Quantity: " + prod.quantity, 20);
+		pdf.fontSize(10).text("Unit price: " + prod.price, 20);
+		total += parseFloat(prod.quantity) * parseFloat(prod.price, 20);
+		pdf.text("=================================", 20);
 	});
 	pdf.text(" ");
 	pdf.fontSize(12).text("Total: £" + total);
 	pdf.end();
 	return pdf;
+};
+
+exports.deleteFile = async filepath => {
+	await fs.unlink(filepath);
 };
