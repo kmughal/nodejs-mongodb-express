8cebb9ae29cbf57387bf45b5c0651186c3971b54
Khurram Shahzad <macpro@macpros-MBP-2.home>
2019-04-21 14:48:11 +0100

    Add method to generate pdf

--- common/invoice-helpers.js
+++ common/invoice-helpers.js
@@ -1,17 +1,37 @@
 const fs = require("fs");
 const path = require("path");
-const { promisify } = require("util");
-const fileExists = promisify(fs.exists);
 
-exports.getInvoiceStream = async filename => {
-	try {
-		const invoiceFileFullPath = path.resolve(__dirname, "../invoices",filename);
-		console.log("Invoice path:", invoiceFileFullPath);
-		// const invoiceExists = await fileExists(invoiceFileFullPath);
-		// if (!invoiceExists) new Error(`${filename} not found`);
-		
-		return fs.createReadStream(invoiceFileFullPath);
-	} catch (e) {
-		throw e;
-	}
+
+const PdfDocument = require("pdfkit");
+
+exports.generateInvoice = ({ orderId, products }) => {
+	const invoiceFileFullPath = path.resolve(
+		__dirname,
+		`../invoices/invoice-${orderId}.pdf`
+	);
+
+	console.log(invoiceFileFullPath);
+	const pdf = new PdfDocument();
+	pdf.pipe(fs.createWriteStream(invoiceFileFullPath));
+
+	
+	pdf
+		.fontSize(8)
+		.text("Date:" + new Date().toLocaleDateString("en-gb"), 400, 10);
+	pdf.fontSize(15).text("Invoice", 10, 50);
+
+	let total = 0.0;
+	pdf.fontSize(10);
+	pdf.text("=================================",20);
+	products.forEach(prod => {
+		pdf.fontSize(11).text("Product name: " + prod.title,20);
+		pdf.fontSize(10).text("Quantity: " + prod.quantity,20);
+		pdf.fontSize(10).text("Unit price: " + prod.price,20);
+		total += parseFloat(prod.quantity) * parseFloat(prod.price,20);
+		pdf.text("=================================",20);
+	});
+	pdf.text(" ");
+	pdf.fontSize(12).text("Total: £" + total);
+	pdf.end();
+	return pdf;
 };
